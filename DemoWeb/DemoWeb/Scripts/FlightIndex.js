58a81560791011bdee61c03331140a6194bd50ed
Michael Duncan <mwduncan2018@gmail.com>
2019-06-08 18:42:52 -0400

    updates

---
+++ DemoWeb/DemoWeb/Scripts/FlightIndex.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+$(document).ready(function () {
+    alert("0");
+    var flightRows = document.querySelectorAll("#flightRow");
+
+    flightRows.forEach(function (x) {
+        alert("1");
+        if (x.querySelector("input").checked == true) {
+            alert("2");
+            $(x).addClass("highlightRow");
+        }
+    });
+});
